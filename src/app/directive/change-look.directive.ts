ada3ce46e29b6555048ba1571c99cffa795ea940
Tami Vahab <tm80009000@gmail.com>
2024-07-05 11:05:40 +0300

    BH commit directive 2

--- src/app/directive/change-look.directive.ts
+++ src/app/directive/change-look.directive.ts
@@ -5,17 +5,14 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class ChangeLookDirective {
   @Input() mycolor: string
+  @Input() myText: string
 
   constructor(public element: ElementRef) { }
-  //דירטיב מקבל פרמטר
-  // בשלב ראשון נגדיר בקונסטרקטור את האלמנט שעליו אנו עובדות
+  
   ngOnInit(): void {
     this.element.nativeElement.style.backgroundColor = this.mycolor
   }
-  //אירוע
-  @HostListener('click') h() {
-    this.element.nativeElement.innerHTML = "לחצו עלי"
-  }
+
   @HostListener('mouseenter') K() {
     this.element.nativeElement.style.backgroundColor = "pink"
   }
